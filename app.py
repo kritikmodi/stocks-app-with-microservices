097dc3ed57ef3f5445a049d572f77fc771c95027
kritikmodi <kritikmodi@gmail.com>
2023-07-10 17:17:53 +0530

    Remove unused variable

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ def index1():
 @app.route('/2', methods=['POST'])
 def index2():
     company_code = request.form['text']
-    company_ticker = finance.Ticker(company_code)
     company_finance_details = finance.download(company_code, start="2022-04-01", end="2022-09-01")
     return company_finance_details.to_string(index=True)
 
